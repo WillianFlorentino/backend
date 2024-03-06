963b4736cc1b99253ff17e91fc978adccb7db428
Willian Florentino <williandimi11@gmail.com>
2024-03-05 21:28:28 -0300

    Implementação camada de modelo e persistência.

---
+++ Persistencia/ClienteDAO.js
@@ -0,0 +1,105 @@
+import conectar from "./Conexao.js"; //não esquecer de colocar a extensão .js no final
+import Cliente from "../Modelos/Cliente.js";
+//DAO - Data Access Object
+export default class ClienteDAO{
+    async gravar(cliente){
+        if (cliente instanceof Cliente){
+            const conexao = await conectar();
+            const sql = `INSERT INTO cliente (cpf, nome, usuario, endereco, bairro,
+                         cidade, estado, telefone, idade, rg, email) 
+                         values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+            const parametros = [
+                cliente.cpf,
+                cliente.nome,
+                cliente.usuario,
+                cliente.endereco, 
+                cliente.bairro,
+                cliente.cidade,
+                cliente.estado,
+                cliente.telefone,
+                cliente.idade,
+                cliente.rg,
+                cliente.email
+            ];
+            const [resultados, campos] = await conexao.execute(sql,parametros);
+            //funcionalidade interessante oferecida pela biblioteca mysql2
+            cliente.codigo = resultados.insertId; //recupera o id gerado pelo banco de dados
+        }
+    }
+
+    async atualizar(cliente){
+        if (cliente instanceof Cliente){
+            const conexao = await conectar();
+            const sql = `UPDATE cliente SET cpf = ?,
+                         nome = ?, usuario = ?, endereco = ?, bairro = ?,
+                         cidade = ?, estado = ?, telefone = ?, idade = ?,
+                         rg = ?, email = ? WHERE id = ?`;
+            const parametros = [
+                cliente.cpf,
+                cliente.nome,
+                cliente.usuario,
+                cliente.endereco,
+                cliente.bairro,
+                cliente.cidade,
+                cliente.estado,
+                cliente.telefone,
+                cliente.idade,
+                cliente.rg,
+                cliente.email,
+                cliente.codigo
+            ];
+
+            await conexao.execute(sql,parametros);
+        }
+    }
+
+    async excluir(cliente){
+        if (cliente instanceof Cliente){
+            const conexao = await conectar();
+            const sql = `DELETE FROM cliente WHERE id = ?`;
+            const parametros = [
+                cliente.codigo
+            ]
+            await conexao.execute(sql,parametros);
+        }
+    }
+
+    //termo de pesquisa pode ser o código do cliente ou ainda o nome
+    
+    async consultar(termoDePesquisa){
+        if (termoDePesquisa === undefined){
+            termoDePesquisa = "";
+        }
+        let sql="";
+        if (isNaN(termoDePesquisa)){ //termo de pesquina não é um número
+            sql = `SELECT * FROM cliente WHERE nome LIKE ?`; //like será um parametro
+            termoDePesquisa= '%' + termoDePesquisa + '%';
+        }
+        else{
+            sql = `SELECT * FROM cliente WHERE id = ?`;
+        }
+
+        const conexao = await conectar();
+        const [registros] = await conexao.execute(sql,[termoDePesquisa]);
+        //Utilizar os registros encontrados para criar novos objetos do tipo cliente
+        let listaClientes = [];
+        for (const registro of registros){
+            const cliente = new Cliente(
+                registro.id,
+                registro.cpf,
+                registro.nome,
+                registro.usuario,
+                registro.endereco,
+                registro.bairro,
+                registro.cidade,
+                registro.estado,
+                registro.telefone,
+                registro.idade,
+                registro.rg,
+                registro.email
+            );
+            listaClientes.push(cliente);
+        }
+        return listaClientes;
+    }
+}
